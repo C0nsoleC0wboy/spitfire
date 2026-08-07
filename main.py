f264408f1f194e846b98d69559247964fce6531b
C0nsoleC0wboy <155922548+C0nsoleC0wboy@users.noreply.github.com>
2026-08-07 22:49:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,21 +26,22 @@ if __name__ == '__main__':
         pickle_to_file(prev_ac)
 
     try:
-        ac = live_ac.search_radius(51.30457774177224, -0.09535818745250407, 7)
+        ac = live_ac.search_radius(51.30457774177224, -0.09535818745250407, 10)
     except caa.client.RequestException:
         ac = []
 
     ac_dict = dict(map(lambda x: (x[0], (x[1], x[2])), (filter(lambda x: x if x[0] else None, ac))))
-    ac_set = set(map(lambda x: x[0][2:], filter(lambda x: x if x[0] and x[0][:2] == 'G-' else None, ac)))
+    ac_set = set(map(lambda x: x[0], filter(lambda x: x if x[0] else None, ac)))
 
     alert_ac = ac_set & caa.war_ac
 
     for p in alert_ac:
         if p not in prev_ac:
-            plane_details = ac_dict[f'G-{p}']
+            plane_details = ac_dict[p]
             alert = '🚨 War plane spotted 🚨\n\n' \
-                    f'Plane: {plane_details[0]}\nReg: G-{p}\n\nClick to track 👇 \n'\
+                    f'Plane: {plane_details[0]}\nReg: {p}\n\nClick to track 👇 \n'\
                     f'https://globe.airplanes.live/?icao={plane_details[1]}'
             t.send_message(CHANNEL_ID, alert)
             prev_ac.add(p)
     pickle_to_file(alert_ac & prev_ac)
+    
